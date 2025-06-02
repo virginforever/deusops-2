1da76131b9841a16e01dada2b3b053059b546c85
papakarlo1771 <misemen4@gmail.com>
2025-06-02 13:32:15 +0500

    добавил healtcheck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -10,11 +11,38 @@ DB_NAME = os.environ.get('DB_NAME', 'mydb')
 DB_USER = os.environ.get('DB_USER', 'user')
 DB_PASSWORD = os.environ.get('DB_PASSWORD', 'password')
 
+def get_db_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+
 @app.route('/')
 def index():
-    #Тут он возвращает информацию о базе данных в формате Json
+    #Теперь возвращается Healthcheck
+    if health():
+        return health() 
+
+
+@app.route('/health')
+def health():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT 1;')
+        cur.close()
+        conn.close()
+        db_status = "ok"
+    except Exception as e:
+        db_status = "fail"
+    
+    overall_status = "ok" if db_status == "ok" else "fail"
     return jsonify(
-        message="My name is Simon Kazantsev",
+        status=overall_status,
+        database_status=db_status,
         database={
             "host": DB_HOST,
             "port": DB_PORT,
